52c44308c41b6f22b3cf2b21de01f10ffb8e2f3d
Manh hung <manhhungptit261@gmail.com>
2018-02-28 23:14:56 +0700

    commit change

---
+++ app/src/main/java/com/example/dell/lifecircle/Main4Activity.java
@@ -0,0 +1,10 @@
+package com.example.dell.lifecircle;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Dell on 2/28/2018.
+ */
+
+public class Main4Activity extends AppCompatActivity {
+}
